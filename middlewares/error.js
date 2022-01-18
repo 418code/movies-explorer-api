394650a4749935fc46a0953899d6beaf9f2c7fe4
Maxim Tolstokory <maxim.code@outlook.com>
2022-01-17 18:22:19 -0900

    feat: add error logic

---
+++ middlewares/error.js
@@ -0,0 +1,20 @@
+const { isCelebrateError } = require('celebrate');
+const {
+  sendErrRes,
+  errCodes,
+  errMsgs,
+  errors,
+} = require('../utils/utils');
+
+module.exports.handleErrors = (err, req, res) => {
+  const errorNames = Object.keys(errors);
+
+  // catch celebrate validation errors
+  if (isCelebrateError(err)) {
+    sendErrRes(res, errCodes.ERR_CODE_BAD_DATA, errMsgs.ERR_MSG_BAD_DATA('entered'));
+  } else if (errorNames.includes(err.name)) {
+    sendErrRes(res, errors[err.name], err.message);
+  } else {
+    sendErrRes(res, errCodes.ERR_CODE_DEFAULT, errMsgs.ERR_MSG_DEFAULT);
+  }
+};
---
+++ utils/utils.js
@@ -0,0 +1,47 @@
+module.exports.errMsgs = {
+  ERR_MSG_DEFAULT: 'A server error happened',
+  ERR_MSG_LOGIN: 'Wrong email or password',
+  ERR_MSG_AUTH_REQ: 'Authentication required',
+  ERR_MSG_FORBIDDEN: 'Not authorized',
+  ERR_MSG_BAD_DATA: (name) => `Bad ${name} data`,
+  ERR_MSG_NOT_FOUND: (name) => `Requested ${name} not found`,
+  ERR_MSG_NOT_CREATED: (name) => `Requested ${name} not created`,
+  ERR_MSG_NOT_UPDATED: (name) => `Requested ${name} not updated`,
+};
+
+module.exports.resMsgs = {
+  RES_MSG_MOVIE_DELETED: 'Movie successfully deleted',
+};
+
+module.exports.errNames = {
+  VALIDATION: 'ValidationError',
+  CAST: 'CastError',
+  CONFLICT: 'ConflictError',
+  NOT_FOUND: 'DocumentNotFoundError',
+  NOT_AUTH: 'AuthenticationError',
+  FORBIDDEN: 'ForbiddenError',
+  MONGO: 'MongoServerError',
+};
+
+module.exports.errCodes = {
+  ERR_CODE_BAD_DATA: 400,
+  ERR_CODE_NOT_AUTH: 401,
+  ERR_CODE_FORBIDDEN: 403,
+  ERR_CODE_NOT_FOUND: 404,
+  ERR_CODE_CONFLICT: 409,
+  ERR_CODE_DEFAULT: 500,
+  ERR_CODE_MDB_DUPLICATE: 11000,
+};
+
+module.exports.errors = {
+  [this.errNames.VALIDATION]: this.errCodes.ERR_CODE_BAD_DATA,
+  [this.errNames.CAST]: this.errCodes.ERR_CODE_BAD_DATA,
+  [this.errNames.NOT_FOUND]: this.errCodes.ERR_CODE_NOT_FOUND,
+  [this.errNames.NOT_AUTH]: this.errCodes.ERR_CODE_NOT_AUTH,
+  [this.errNames.FORBIDDEN]: this.errCodes.ERR_CODE_FORBIDDEN,
+  [this.errNames.CONFLICT]: this.errCodes.ERR_CODE_CONFLICT,
+};
+
+module.exports.sendErrRes = (res, errCode, errMsg) => {
+  res.status(errCode).send({ message: errMsg });
+};
