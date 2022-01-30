6c1d8ca9838042767be028f745cccfd800981d9c
Maxim Tolstokory <maxim.code@outlook.com>
2022-01-29 18:28:46 -0900

    feat: add loggers

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const cookieParser = require('cookie-parser');
 const { handleErrors } = require('./middlewares/error');
 const router = require('./routes/index');
 const limiter = require('./middlewares/limit');
+const {
+  requestLogger,
+  errorLogger,
+} = require('./middlewares/logger');
 
 const { PORT } = process.env;
 
@@ -13,12 +17,14 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
 
 const app = express();
 
+app.use(requestLogger);
 app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '1kb', parameterLimit: 10 }));
 app.use(cookieParser());
 
 app.use(router);
+app.use(errorLogger);
 app.use(handleErrors);
 
 app.listen(PORT);
